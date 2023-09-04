d982683e994be936b9e6bfdb72bc94116e4073af
Brian Kim <2kimbrian1@gmail.com>
2023-09-04 03:52:38 -0700

    channel service shit

--- pkg/messaging/get_channel.go
+++ pkg/messaging/get_channel.go
@@ -1,1 +1,33 @@
 package messaging
+
+import (
+	"context"
+
+	"github.com/projectulterior/2cents-backend/pkg/format"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type GetChannelRequest struct {
+	ChannelID format.ChannelID
+}
+
+type GetChannelResponse = Channel
+
+func (s *Service) GetChannel(ctx context.Context, req GetChannelRequest) (*GetChannelResponse, error) {
+	var channel Channel
+	err := s.Collection(CHANNELS_COLLECTION).
+		FindOne(ctx, bson.M{"_id": req.ChannelID.String()}).
+		Decode(&channel)
+	if err != nil {
+		if err != mongo.ErrNoDocuments {
+			return nil, status.Error(codes.Internal, err.Error())
+		}
+
+		return nil, status.Error(codes.NotFound, err.Error())
+	}
+
+	return &channel, nil
+}
