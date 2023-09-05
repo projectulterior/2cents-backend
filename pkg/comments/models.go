540d416dc13ccdaeaff5468c8d992c06c05d70e5
Brian Kim <2kimbrian1@gmail.com>
2023-09-04 23:58:20 -0700

    comment.go

--- pkg/comments/models.go
+++ pkg/comments/models.go
@@ -1,6 +1,7 @@
 package comments
 
 import (
+	"context"
 	"time"
 
 	"github.com/projectulterior/2cents-backend/pkg/format"
@@ -17,4 +18,8 @@ type Comment struct {
 	AuthorID  format.UserID    `bson:"author_id"`
 	CreatedAt time.Time        `bson:"created_at"`
 	UpdatedAt time.Time        `bson:"updated_at"`
+}
+
+func (s *Service) Setup(ctx context.Context) error {
+	return nil
 }
--- pkg/services/services.go
+++ pkg/services/services.go
@@ -2,12 +2,14 @@ package services
 
 import (
 	"github.com/projectulterior/2cents-backend/pkg/auth"
+	"github.com/projectulterior/2cents-backend/pkg/comments"
 	"github.com/projectulterior/2cents-backend/pkg/posts"
 	"github.com/projectulterior/2cents-backend/pkg/users"
 )
 
 type Services struct {
-	Auth  *auth.Service
-	Users *users.Service
-	Posts *posts.Service
+	Auth     *auth.Service
+	Users    *users.Service
+	Posts    *posts.Service
+	Comments *comments.Service
 }
